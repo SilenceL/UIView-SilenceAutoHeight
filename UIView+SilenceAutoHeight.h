8eac4d0ee03b8e0e9ebcd5b305b2112994c45ee1
Silence Liu <silence_liu163@163.com>
2016-10-02 07:31:41 +0800

    Add files via upload

---
+++ UIView+SilenceAutoHeight.h
@@ -0,0 +1,23 @@
+//
+//  UIView+SilenceAutoHeight.h
+//
+//  Created by 刘西南 on 16/9/30.
+//  Copyright © 2016年 Silence. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (SilenceAutoHeight)
+
+/**
+ 自动计算自动布局下的高度，需要将约束设满，可参考
+ https://github.com/forkingdog/UITableView-FDTemplateLayoutCell
+ 里面的设置约束的说明
+
+ @param view  需要计算行高的视图
+ @param width 需要计算行高的视图的宽度
+
+ @return 返回计算完成的高度
+ */
++(CGFloat)autoLayoutHeightWithView:(UIView *)view andWidth:(CGFloat)width;
+@end
---
+++ UIView+SilenceAutoHeight.m
@@ -0,0 +1,24 @@
+//
+//  UIView+SilenceAutoHeight.h
+//
+//  Created by 刘西南 on 16/9/30.
+//  Copyright © 2016年 Silence. All rights reserved.
+//
+
+#import "UIView+SilenceAutoHeight.h"
+
+@implementation UIView (SilenceAutoHeight)
++(CGFloat)autoLayoutHeightWithView:(UIView *)view andWidth:(CGFloat)width{
+    
+    CGFloat contentViewWidth = width;
+    CGSize fittingSize = CGSizeZero;
+    // Add a hard width constraint to make dynamic content views (like labels) expand vertically instead
+    // of growing horizontally, in a flow-layout manner.
+    NSLayoutConstraint *tempWidthConstraint = [NSLayoutConstraint constraintWithItem:view attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0 constant:contentViewWidth];
+    [view addConstraint:tempWidthConstraint];
+    // Auto layout engine does its math
+    fittingSize = [view systemLayoutSizeFittingSize:UILayoutFittingCompressedSize];
+    [view removeConstraint:tempWidthConstraint];
+    return fittingSize.height;
+}
+@end
